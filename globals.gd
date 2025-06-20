a4d939dd795b95e9fa9312af2cc22be66205ec1e
Erica Macalino <99041869+macaroneriii@users.noreply.github.com>
2025-06-20 12:55:35 +0800

    Add files via upload

---
+++ globals.gd
@@ -0,0 +1,17 @@
+# res://globals.gd
+extends Node
+
+var dishes_data = {}
+var themes_data = {}
+
+
+var selected_dish_name: String = ""
+var player_meat_cards: Array = []
+var ai_meat_cards: Array = []
+var meat_battle_results: Dictionary = {}
+var player_ingredient_cards: Array = []
+var ai_ingredient_cards: Array = []
+var player_cooking_cards: Array = []
+var ai_cooking_cards: Array = []
+var cooking_battle_results: Dictionary = {}
+var ingredients_battle_results: Dictionary = {}
